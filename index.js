ea0a5741c698d89a4629c824a406a8326aae2ee8
Monica Tamaru Jones <mntamaru@gmail.com>
2019-11-11 18:56:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 // Code your solution here!
 
+function printString() {
+
+  console.log(myString[0]);
+  
+  if (myString.length >1) {
+    let mySubString = myString.substring(1, myString.length);
+    pringString(mySubString);
+  }
+  else {
+    return true;
+  }
+}
